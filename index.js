a924e66b7c12ee1e20296510dddc7b13ceb8e66b
Ayush <ayush.ramkuar@gmail.com>
2017-11-01 20:19:01 -0400

    added file queueing and receiving

--- index.js
+++ index.js
@@ -5,27 +5,37 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(fileUpload());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post('/upload', function(req, res) {
-  console.log(JSON.parse(req.files.form.data)); // the uploaded file object
-});
-
 const amqp = require('amqplib/callback_api');
+
+app.get('/', (request, response) => {
+  response.sendFile(`${__dirname}/` + 'index.html');
+});
 
 amqp.connect('amqp://cdllbwik:zTqDj6ZKHD4EM4IYRCNmcGDEDb3Lrjgt@mosquito.rmq.cloudamqp.com/cdllbwik', (err, conn) => {
   conn.createChannel((err, ch) => {
+    console.log('Connected to cloudAMPQ');
+
+    app.post('/upload', function (req, res) {
+      const jsonArray = JSON.parse(req.files.form.data);
+
+      jsonArray.forEach(function (queue) {
+        ch.assertQueue(queue.queueName, { durable: false });
+
+        queue.items.forEach((element) => {
+          ch.sendToQueue(queue.queueName, new Buffer(element.toString()));
+        }, this);
+
+        console.log('Sent to %s', queue.queueName);
+      }, this);
+    });
+
     const q = 'queue2';
-    ch.assertQueue(q, { durable: false });
 
     app.post('/', (req, res) => {
       ch.sendToQueue(q, new Buffer(req.body.text));
       console.log('Sent Message');
     });
   });
-});
-
-app.get('/', (request, response) => {
-  response.sendFile(`${__dirname}/` + 'index.html');
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
--- receive.js
+++ receive.js
@@ -1,14 +1,19 @@
+const prompt = require('prompt');
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://cdllbwik:zTqDj6ZKHD4EM4IYRCNmcGDEDb3Lrjgt@mosquito.rmq.cloudamqp.com/cdllbwik', function(err, conn) {
+prompt.start();
+prompt.get('queueName', function(err, result) {
+  const queue = result.queueName;
+
+  amqp.connect('amqp://cdllbwik:zTqDj6ZKHD4EM4IYRCNmcGDEDb3Lrjgt@mosquito.rmq.cloudamqp.com/cdllbwik', function(err, conn) {
     conn.createChannel(function(err, ch) {
-      var q = 'queue2';
-      ch.assertQueue(q, {durable: false});
+      ch.assertQueue(queue, {durable: false});
       
-      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
-      ch.consume(q, function(msg) {
-        console.log(" [x] Received %s", msg.content.toString());
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+      ch.consume(queue, function(msg) {
+        console.log(" [x] Received " + msg.content.toString());
       }, {noAck: true});
-      
     });
   });
+});
+
